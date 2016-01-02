d3ec2bd127a2a61ee8fc7edb03ff4eb6ccd46b63
pakhandi <asimkprasad@gmail.com>
2016-01-02 13:44:36 +0530

    CleanUp

--- ini.py
+++ ini.py
@@ -8,7 +8,6 @@ def hammingDistance(s1, s2):
     """Return the Hamming distance between equal-length sequences"""
     if len(s1) != len(s2):
     	return 100
-        raise ValueError("Undefined for sequences of unequal length")
     return sum(bool(ord(ch1) - ord(ch2)) for ch1, ch2 in zip(s1, s2))
 
 if len(sys.argv)!=3:
